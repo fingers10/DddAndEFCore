2c51dd096873d0e48c4ccf43a902781b3d1b745b
Abdul Rahman <43729469+fingers10@users.noreply.github.com>
2020-05-08 14:50:22 +0530

    Update Program.cs

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -10,14 +10,16 @@ namespace App
     {
         public static void Main()
         {
-            string result5 = Execute(x => x.EditPersonalInfo(13, "Carl 2", "Carlson 2", 2, "carl1@gmail.com", 1));
+            var result5 = Execute(x => x.AddAnnotation());
+            var result6 = Execute(x => x.UpdateAnnotation());
+            //string result5 = Execute(x => x.EditPersonalInfo(13, "Carl 2", "Carlson 2", 2, "carl1@gmail.com", 1));
             //string result4 = Execute(x => x.RegisterStudent("Carl", "carl@gmail.com", 2, Grade.B));
             //string result3 = Execute(x => x.DisenrollStudent(1, 2));
             //string result = Execute(x => x.CheckStudentFavoriteCourse(1, 2));
             //string result2 = Execute(x => x.EnrollStudent(1, 2, Grade.A));
         }
-
-        private static string Execute(Func<StudentController, string> func)
+        
+        private static string Execute(Func<AnnotationsController, string> func)
         {
             string connectionString = GetConnectionString();
 
@@ -27,10 +29,25 @@ namespace App
 
             using (var context = new SchoolContext(connectionString, true, eventDispatcher))
             {
-                var controller = new StudentController(context);
+                var controller = new AnnotationsController(context);
                 return func(controller);
             }
         }
+
+        //private static string Execute(Func<StudentController, string> func)
+        //{
+        //    string connectionString = GetConnectionString();
+
+        //    IBus bus = new Bus();
+        //    var messageBus = new MessageBus(bus);
+        //    var eventDispatcher = new EventDispatcher(messageBus);
+
+        //    using (var context = new SchoolContext(connectionString, true, eventDispatcher))
+        //    {
+        //        var controller = new StudentController(context);
+        //        return func(controller);
+        //    }
+        //}
 
         private static string GetConnectionString()
         {
